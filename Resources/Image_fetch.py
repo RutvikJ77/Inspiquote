8279661c9fef8c66985caae477aebe3f9eb368e7
Rutvik J <rutvikj77@outlook.com>
2021-01-22 20:00:33 +0530

    Chore: Updated getImage class

--- Resources/Image_fetch.py
+++ Resources/Image_fetch.py
@@ -1,4 +1,5 @@
 import requests
+import random
 import urllib.request as url
 from PIL import Image
 
@@ -8,31 +9,28 @@ UNSPLASH_ACCESS_KEY = 'VmQr4vB4YR8eeRKokj3HRWQjPK2tlRJ-qI3ZjblP-sc'
 
 class GetImage:
     
-    def get_unsplash_img(self,query_word = 'motivation')
+    def get_unsplash_img(self,query_word = 'motivation'):
         """
         Gets images from Unsplash and downloads+resizes it.
         @param query_word - ranked word
         returns list of metrics
         """
         orientation =random.choice(['landscape','portrait'])
-        response = requests.get('https://api.unsplash.com/photos/random',
-                            headers={'Authorization':'Client-ID '+UNSPLASH_ACCESS_KEY},params={'query':query_word,'orientation':'portrait'})
-        img_data = response.json()
-        img_url = img_data['links']['download']
+        if orientation == "landscape":
+            response = requests.get('https://api.unsplash.com/photos/random',
+                        headers={'Authorization':'Client-ID '+UNSPLASH_ACCESS_KEY},params={'query':query_word,'orientation':orientation})
+            img_data = response.json()
+            img_url = img_data['urls']['raw'] + "&fit=max&w=1200&h=675"
+        else:
+            response = requests.get('https://api.unsplash.com/photos/random',
+                        headers={'Authorization':'Client-ID '+UNSPLASH_ACCESS_KEY},params={'query':query_word,'orientation':orientation})
+            img_data = response.json()
+            img_url = img_data['urls']['raw'] + "&fit=max&w=640&h=960"
+
         img_author = img_data['user']['name']
         img_twitter_username = img_data['user']['twitter_username']
-        img_name = query+'.jpg'
+        img_name = 'bg images/1.jpg'
         url.urlretrieve(img_url,img_name)
-
-        "Resizing Image"
-        if orientation =='landscape':
-            bg=Image.open(img_name)
-            resize_bg = bg.resize((1200,675))
-            resize_bg.save('bg images/resize_bg.jpg')
-        else:
-            bg=Image.open(img_name)
-            resize_bg = bg.resize((640,960))
-            resize_bg.save('bg images/resize_bg.jpg')
 
         return [img_author,img_twitter_username,orientation]
 
