2c6333e46efb5b0c1cdd41a0d0385e180ce752e5
Rutvik J <rutvikj77@outlook.com>
2021-01-22 19:58:29 +0530

    Feature: Image editing

--- post.py
+++ post.py
@@ -1,7 +1,8 @@
-#import tweepy
 from Resources.quotes_fetch import Quotes
 from Resources.Image_fetch import GetImage
-
+import Resources.Image_edit as image_edit
+import random
+import tweepy
 
 
 CONSUMER_KEY ="a0S4RKnB0kkH5ON4hrIuj6wKY"
@@ -13,18 +14,26 @@ ACCESS_TOKEN_SECRET ="LHSK4PFQV2C2F2iuhXxXTgVZjHxSyyvSTafwo6cMnnbkZ"
 # image_path = 'test.jpg'
 
 # Get the quote details
-def quote_post():
-    quote = Quotes()
-    today_quote = quote.quotable()
-    print("Quote - " + today_quote[0])
-    print("Author - " + today_quote[1])
+
+quote = Quotes()
+today_quote = random.choice([quote.quotable(),quote.quotes_fav()])
+query = quote.word_query_image(today_quote[0])
+print("Quote - " + today_quote[0])
+print(query)
 
 
 # Retrieve the image details
 
+bg = GetImage()
+bg_image_details = bg.get_unsplash_img(query)
+print(bg_image_details)
 
 
 
+# Image Editing
+quote = "\"" + today_quote[0] + "\""
+author = "- " + today_quote[1]
+image_edit(bg_image_details[2],quote,author)
 
 
 
@@ -39,8 +48,9 @@ def quote_post():
 
 
 
+
 # auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET) 
 # auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 # api=tweepy.API(auth)
-# tweet = '''The Sky is not the limit. Your mind is. - L.J Vanier\n.\n.\n 📸: Batu Gezer'''
+# tweet = today_quote[0] + "\n.\n.\n 📸:"
 # status = api.update_with_media(image_path, tweet)
