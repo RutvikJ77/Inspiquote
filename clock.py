2abc17cd00e7f9903fbde0b8d6ee9a45a15c08ab
Rutvik J <rutvikj77@outlook.com>
2021-01-27 20:58:21 +0530

    Chore: update to callable

--- clock.py
+++ clock.py
@@ -3,12 +3,12 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 sched = BlockingScheduler()
-sched.add_job(tags(api),'interval',seconds=60)
+sched.add_job(tags,'interval',seconds=60)
 
-sched.add_job(message(api),'interval',minutes=2)
+sched.add_job(message,'interval',minutes=2)
 
-sched.add_job(retweet_fun(api),'interval',hours=8)
+sched.add_job(retweet_fun,'interval',hours=8)
 
-sched.add_job(posting(api),'cron', day_of_week='mon-sun', hour=9)
+sched.add_job(posting,'cron', day_of_week='mon-sun', hour=9)
 
 sched.start()
