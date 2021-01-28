8956757d1ea460c7f95d78bd088cfb1d48fbdcd7
Rutvik J <rutvikj77@outlook.com>
2021-01-28 15:29:04 +0530

    Chore: Timetest

--- clock.py
+++ clock.py
@@ -3,12 +3,12 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 sched = BlockingScheduler()
-sched.add_job(tags,'interval',minutes=1)
+sched.add_job(tags,'interval',minutes=3)
 
 sched.add_job(message,'interval',minutes=4)
 
 sched.add_job(retweet_fun,'interval',hours=8)
 
-sched.add_job(post,'cron', day_of_week='mon-sun', hour=9)
+sched.add_job(post,'cron', day_of_week='mon-sun', hour=3,minute=30)
 
 sched.start()
