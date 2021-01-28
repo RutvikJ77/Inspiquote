d596d15b695e5a5912a1bf9490ec2bf9819248a9
Rutvik J <rutvikj77@outlook.com>
2021-01-28 16:29:01 +0530

    Chore: time update

--- clock.py
+++ clock.py
@@ -9,6 +9,6 @@ sched.add_job(message,'interval',minutes=4)
 
 sched.add_job(retweet_fun,'interval',hours=8)
 
-sched.add_job(post,'cron', day_of_week='mon-sun', hour=16,minute=20)
+sched.add_job(post,'cron', day_of_week='mon-sun', hour=16,minute=35)
 
 sched.start()
