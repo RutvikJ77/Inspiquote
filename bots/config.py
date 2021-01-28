b8650e2a542e8d982ff81b5473865bf5aee0fb98
Rutvik J <rutvikj77@outlook.com>
2021-01-28 15:59:25 +0530

    Chore: Test time

--- bots/config.py
+++ bots/config.py
@@ -1,13 +1,12 @@
-from credentials import *
 import logging
 import tweepy
 from os import environ
 logger = logging.getLogger()
 
-# CONSUMER_KEY = environ["CONSUMER_KEY"]
-# CONSUMER_SECRET = environ["CONSUMER_SECRET"]
-# ACCESS_TOKEN = environ["ACCESS_TOKEN"]
-# ACCESS_TOKEN_SECRET = environ["ACCESS_TOKEN_SECRET"]
+CONSUMER_KEY = environ["CONSUMER_KEY"]
+CONSUMER_SECRET = environ["CONSUMER_SECRET"]
+ACCESS_TOKEN = environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = environ["ACCESS_TOKEN_SECRET"]
 
 def create_api():
     """
