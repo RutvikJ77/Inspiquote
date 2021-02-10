30e41d1e631c7dda19e165ca04951f5579b493c2
Rutvik J <rutvikj77@outlook.com>
2021-02-10 12:50:00 +0530

    Chore: Update schedule

--- clock.py
+++ clock.py
@@ -3,11 +3,13 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 sched = BlockingScheduler()
-sched.add_job(tags,'interval',minutes=3)
+sched.add_job(tweet_quote,'interval',hours=6)
 
-sched.add_job(message,'interval',minutes=4)
+sched.add_job(tags,'interval',hours=5)
 
-sched.add_job(retweet_fun,'interval',hours=8)
+sched.add_job(message,'interval',hours=4)
+
+sched.add_job(retweet_fun,'interval',hours=24)
 
 sched.add_job(post,'cron', day_of_week='mon-sun',hour=9)
 
