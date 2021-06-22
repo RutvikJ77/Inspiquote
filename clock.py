247016c17f2ff071625a0a7daaf34debb7149616
Rutvik J <rutvikj77@outlook.com>
2021-06-22 13:35:47 +0530

    feat: Schedule updates

--- clock.py
+++ clock.py
@@ -1,16 +1,9 @@
-from main import *
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+from main import tags,tweet_quote,message,post
 
 sched = BlockingScheduler()
-sched.add_job(tweet_quote,'interval',hours=6)
-
-sched.add_job(tags,'interval',hours=5)
-
-sched.add_job(message,'interval',hours=4)
-
-sched.add_job(retweet_fun,'interval',hours=24)
-
-sched.add_job(post,'cron', day_of_week='mon-sun',hour=9)
-
+sched.add_job(tags, 'interval', hours=5)
+sched.add_job(message, 'interval', hours=4)
+sched.add_job(tweet_quote, 'interval', hours=12)
+sched.add_job(post, 'cron', day_of_week='mon-sun', hour=9)
 sched.start()
