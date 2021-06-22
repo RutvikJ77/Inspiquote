359416e1446b0f4a511aa4d1b1f35f10c1fba410
Rutvik J <rutvikj77@outlook.com>
2021-06-22 13:35:10 +0530

    refactor: Main function updates

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+"""
+Provides main system to run.
+"""
+
+import random
 from Resources.quotes_fetch import Quotes
 from bots.config import create_api
 from post import posting
 from bots.tags import check_mentions
 from bots.direct_message import direct_message_initial
 from bots.retweet_fav import retweet_fav_post
-import time
-import random
+
 
 api = create_api()
 quote_class = Quotes()
@@ -14,17 +18,32 @@ quote = "\"" + quote_author[0] + "\" " + "- " + quote_author[1]
 
 
 def tags():
+    """
+    Checks for tags, if any.
+    """
     last_id = 1
     last_id = check_mentions(api,last_id,quote)
 
 def message():
+    """
+    Sends a direct message.
+    """
     direct_message_initial(api)
 
 def tweet_quote():
+    """
+    Updates Tweet Quotes at scheduled interval.
+    """
     api.update_status(quote)
 
 def post():
+    """
+    Posts the image quotes.
+    """
     posting(api)
 
 def retweet_fun():
+    """
+    Retweets if there are any particular hastags.
+    """
     retweet_fav_post(api)
